e64a0ff7d6ff9b2c0c8e9f98e75566b3cbbaf25d
amartejak <amarteja2@gmail.com>
2024-04-01 01:42:39 -0500

    updated comments

--- src/redux/pokeAction.ts
+++ src/redux/pokeAction.ts
@@ -1,16 +1,22 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit"; // Import createAsyncThunk from Redux Toolkit
+import axios from "axios"; // Import axios for making HTTP requests
 
-
+// Define an async thunk to fetch Pokemon data from the API
 export const getPokeData = createAsyncThunk("getPokeData", async () => {
-
-    try{
+    try {
+        // Make a GET request to the Pokemon API
         const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
+        
+        // Log the response data to the console
         console.log(response.data);
+        
+        // Return the fetched data
         return response.data;
-    } catch(err){
+    } catch (err) {
+        // Log an error message if fetching data fails
         console.log("Could not fetch data from API", err);
-        return []
+        
+        // Return an empty array if an error occurs
+        return [];
     }
-    
 });
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,16 @@
-import { configureStore, createReducer } from '@reduxjs/toolkit'
-import  cartSlice  from './counterReducer'
-import  pokeSlice  from './pokeReducer'
+import { configureStore, createReducer } from '@reduxjs/toolkit';
+import cartSlice from './counterReducer'; // Import the cart slice
+import pokeSlice from './pokeReducer'; // Import the poke slice
 
-
+// Configure the Redux store with combined reducers
 export const store = configureStore({
   reducer: {
-    poke: pokeSlice,
-    cart: cartSlice
+    poke: pokeSlice, // Set the poke slice reducer under the 'poke' key
+    cart: cartSlice, // Set the cart slice reducer under the 'cart' key
   },
-})
+});
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+// Infer the RootState and AppDispatch types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {poke: PokeState, cart: CartState}
+export type AppDispatch = typeof store.dispatch;
